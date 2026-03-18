33889a0bd565b97e3c74b7e59bb3359dee60b724
112534079-design <112534079@kanchiuniv.ac.in>
2026-03-18 16:13:48 +0530

    Add files via upload

---
+++ four.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class four
+{
+static public void main(String[]args)throws Exception
+{
+Scanner s1=new Scanner(System.in);
+System.out.println("Enter your name");
+String s2=s1 nextLine();
+System.out.println("your name is"+ s2);
+}
+}
