6c3687b96127478836b806c519ea5cb99d1e8450
112534079-design <112534079@kanchiuniv.ac.in>
2026-03-21 10:28:02 +0530

    Add files via upload

---
+++ six.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class six
+{
+static public void main (String[]args)throws Exception
+{
+Scanner Input = new Scanner(System.in);
+int a,b,c;
+System.out.println("Enter value for a");
+a = input.nextInteger();
+System.out.println("Enter value for b");
+b = input.nextInteger();
+System.out.println("Addition of two number");
+c=a+b;
+System.out.println(""+c);
+System.out.println("Subtraction of two number");
+c=a-b;
+System.out.println(""+c);
+System.out.println("Multiplication of two number");
+c=a*b;
+System.out.println(""+c);
+System.out.println("Division of two number");
+c=a/b;
+System.out.println(""+c);
+}
+}
